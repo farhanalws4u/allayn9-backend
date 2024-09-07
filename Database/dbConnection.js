30ef58be9dc40d2da076fc6eb41ab1fc4672ceca
farhaalws4u <farhan.alws4u247@gmail.com>
2024-09-07 12:59:58 +0530

    created auth api

---
+++ Database/dbConnection.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export function dbConnection() {
+  mongoose
+    .connect(
+      process.env.MONGO_URL
+    )
+    .then(() => {
+      console.log("DB Connected Succesfully");
+    })
+    .catch((error) => {
+      console.log("DB Failed to connect", error);
+    });
+}
+
+
+//Use this is postman https://ecommerce-backend-codv.onrender.com/api/v1/auth/signup
+
