80574f9bb2eb32352c65eb2ccde68f586fbbb18e
farhaalws4u <farhan.alws4u247@gmail.com>
2024-09-08 12:05:29 +0530

    done with almost half of the work

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ import { bootstrap } from "./src/bootstrap.js";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from 'cors'
-import { createOnlineOrder } from "./src/modules/order/order.controller.js";
+// import { createOnlineOrder } from "./src/modules/order/order.controller.js";
 
 dotenv.config();
 const app = express();
 app.use(cors())
 
 const port = 8000;
-app.post('/webhook', express.raw({type: 'application/json'}),createOnlineOrder );
+// app.post('/webhook', express.raw({type: 'application/json'}),createOnlineOrder );
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("uploads"));
